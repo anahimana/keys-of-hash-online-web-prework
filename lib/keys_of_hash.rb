7c76ea527125a111df705e65825e4b99507df488
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-07 20:26:05 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
   def keys_of(*arguments)
     # code goes here
     my_result = []
-    # my_hash = to_h
+    my_hash = to_h
     
     # my_hash.each do |key, value|
     #   if value == arguments
