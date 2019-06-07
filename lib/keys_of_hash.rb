1d3ed12aedbe4647dccc473b843d8b49b2284a54
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-07 20:28:03 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -11,7 +11,7 @@ class Hash
     #   end
     # end
     my_hash.each do |key, value|
-      arguments.each {|item| if my_result << key value == arguments }
+      arguments.each {|item| if my_result << key value == item }
     end
     binding.pry
     my_result
