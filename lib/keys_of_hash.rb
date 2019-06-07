be7a9fc3d69175cede48e12c8a4ca81cf9d6d4f2
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-07 20:29:29 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,18 +4,8 @@ class Hash
     # code goes here
     my_result = []
     my_hash = to_h
-    
-    # my_hash.each do |key, value|
-    #   if value == arguments
-    #     my_result << key
-    #   end
-    # end
-    
     my_hash.each do |key, value|
       arguments.each {|item| my_result << key if value == item }
-    end
-    
-    # binding.pry
-    my_result
+    end    my_result
   end
 end
